4d564fd3673b0ac82b52118fed0374c86783d84f
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-05-04 15:55:49 +0200

    updated to fcl 1.0

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -10,8 +10,8 @@
     </li>
   </ul>
   <ul>
-    <li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-    <li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
+    <li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+    <li class:active={$page.url.pathname === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
   </ul>
 </nav>
 
