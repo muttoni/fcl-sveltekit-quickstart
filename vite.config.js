c09a3449f33e731572be3a9f8b054b8d965860e4
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2023-01-16 17:04:22 +0100

    fixed bug thanks to @bluesign

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,25 @@ import inject from '@rollup/plugin-inject'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	define: {
-		global: 'globalThis'
-	},
-	plugins: [
-		nodePolyfills({
+    define: {
+        global: 'globalThis'
+    },
+    plugins: [
+        nodePolyfills({
       // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
     }),
-		sveltekit()
-	],
-	build: {
-		rollupOptions: {
-			plugins: [inject({ Buffer: ['buffer', 'Buffer'] })]
-		},
-	},
+        sveltekit()
+    ],
+    resolve:{
+        alias:{
+            "node-fetch": "./node_modules/node-fetch/browser.js"
+        }
+    },
+    build: {
+        rollupOptions: {
+            plugins: [inject({ Buffer: ['buffer', 'Buffer'] })]
+        },
+    },
 };
 export default config;
